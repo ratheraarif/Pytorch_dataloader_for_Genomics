1347dc3330c1c4ea0066e0fcf1042dec1c8725fe
RA <46924431+ratheraarif@users.noreply.github.com>
2025-10-05 15:46:36 -0400

    Add files via upload

--- generate_data.sh
+++ generate_data.sh
@@ -1,15 +1,16 @@
-out_put="./outputs2"  # define the output folder
+out_put="./train_data"  # define the output folder
 
-python3 basenji_data_h5.py \
+python basenji_data_h5.py \
     -p 15 \
     -w 128 \
     -l 196608 \
+    -s 0.1 \
     -v chr2,chr18,chr22 \
     -t chr1 \
     --crop 40960 \
     --local \
     -o $out_put \
-    ./hg19.ml.fa \
-    ./heart_wigs.txt
+    ./data/hg19.ml.fa \
+    ./data/heart_wigs.txt
 
-python3 process_h5.py out_put/h5py/
+python process_h5.py $out_put/h5py/
